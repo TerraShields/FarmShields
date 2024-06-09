5fe06d114e75ec8eefaf352518323bb1dc702965
Muhammad Habibi Ramadhan <habibiramadhan.dev@gmail.com>
2024-06-09 12:34:07 +0700

    add-anita-ui

---
+++ app/src/main/java/com/gagak/farmshields/ui/anita/AnitaFragment.kt
@@ -0,0 +1,13 @@
+package com.gagak.farmshields.ui.anita
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gagak.farmshields.R
+
+
+class AnitaFragment : Fragment() {
+
+}
